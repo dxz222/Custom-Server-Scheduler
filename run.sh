4021ec9c31b9d762f61c96b6add8fe60c5bbcfc6
dxz222 <96250594+dxz222@users.noreply.github.com>
2022-08-16 16:26:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!bin/bash
-#SBATCH --account=dxz2019
-#SBATCH --output=data/run.log
-#SBATCH --error=data/run.log
+#SBATCH --account=dxz
+#SBATCH --output=data/run_out.log
+#SBATCH --error=data/run_error.log
 #SBATCH --job-name=run
 
 python script.py
