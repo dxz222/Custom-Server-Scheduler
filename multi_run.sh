e635fa3a0c6625328425b18d3dfcca27cb3b67da
dxz222 <96250594+dxz222@users.noreply.github.com>
2022-08-16 16:28:45 +0800

    Update multi_run.sh

--- multi_run.sh
+++ multi_run.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 #SBATCH --account=dxz222
-#SBATCH --output=data/run_out_I.log
-#SBATCH --error=data/run_error_I.log
+#SBATCH --output=run_out_I.log
+#SBATCH --error=run_error_I.log
 #SBATCH --job-name=run_I
 
 python script.py I
